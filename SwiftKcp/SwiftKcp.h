17e62abd594be96a35de2c1f250d68623bdb35e5
peng yunchou <aixinyunchou@icloud.com>
2017-02-08 17:17:27 +0800

    Initial Commit

---
+++ SwiftKcp/SwiftKcp.h
@@ -0,0 +1,19 @@
+//
+//  SwiftKcp.h
+//  SwiftKcp
+//
+//  Created by 彭运筹 on 2017/2/8.
+//  Copyright © 2017年 peng yun chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftKcp.
+FOUNDATION_EXPORT double SwiftKcpVersionNumber;
+
+//! Project version string for SwiftKcp.
+FOUNDATION_EXPORT const unsigned char SwiftKcpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKcp/PublicHeader.h>
+
+
